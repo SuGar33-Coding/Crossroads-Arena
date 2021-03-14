67d1b16002aa873328af0b5523e6905b159ae8cc
tsbraun1891 <tsbraun1891@gmail.com>
2021-03-13 19:03:10 -0500

    Add player experience and levels Add UI for player XP

--- Actors/Generic/hitbox.gd
+++ Actors/Generic/hitbox.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+class_name Hitbox
+
 export var damage = 1
 export var knockbackValue = 475
 
--- Weapons/Arrow.gd
+++ Weapons/Arrow.gd
@@ -1,14 +1,19 @@
 extends KinematicBody2D
 
 export(int) var SPEED = 400
+
+onready var hitbox = $hitbox
+
 var velocity = Vector2.ZERO
+var playerArrow = false
 
 func _physics_process(delta):
 	var test = move_and_collide(velocity * delta)
 	if test != null:
 		self.queue_free()
 
-func fire(startingPosition : Vector2, startingRotation : float):
+func fire(startingPosition : Vector2, startingRotation : float, fromPlayer := false):
+	playerArrow = fromPlayer
 	self.global_position = startingPosition
 	self.global_rotation = startingRotation
 	velocity =  Vector2(1,0).rotated(self.global_rotation) * SPEED
@@ -16,4 +21,6 @@ func fire(startingPosition : Vector2, startingRotation : float):
 
 func _on_hitbox_area_entered(area):
 	if not area.is_in_group("RoomStuff"):
+		if playerArrow:
+			PlayerStats.currentXP += hitbox.damage
 		self.queue_free()
