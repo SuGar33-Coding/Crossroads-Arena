231e18a57d9bfa5056ee0c3dff22c135b2cdf3ec
Gabriel Klavans <gabeklav@bu.edu>
2021-03-27 01:23:54 -0400

    Add bush leaf fx and broken tree fx

--- World/Scenery/Scenery.gd
+++ World/Scenery/Scenery.gd
@@ -2,9 +2,13 @@ extends Area2D
 
 onready var animationPlayer := $AnimationPlayer
 onready var collider := $CollisionShape2D
+onready var shakeFx: Particles2D = $Particles2D 
 
 func _ready():
 	self.connect("body_entered", self, "_play_shake")
 
 func _play_shake(_body):
-	animationPlayer.play("Shake")
+	if animationPlayer != null:
+		animationPlayer.play("Shake")
+	if shakeFx != null:
+		shakeFx.emitting = true
