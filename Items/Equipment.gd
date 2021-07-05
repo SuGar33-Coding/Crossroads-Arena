230dc43782663777ad0d5835223c4df597d6bc90
Gabriel Klavans <gabeklav@gmail.com>
2021-07-05 11:00:13 -0400

    Start on resourcifying items

---
+++ Items/Equipment.gd
@@ -0,0 +1,7 @@
+class_name Equipment extends Item
+
+enum EquipmentType {
+	HEAD, CHEST, FEET
+}
+
+export (EquipmentType) var equipmentType
---
+++ Items/ItemInstance.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+export (Resource) var item
+
+onready var sprite := $Sprite
+
+
+func _ready():
+	sprite.texture = item.texture
