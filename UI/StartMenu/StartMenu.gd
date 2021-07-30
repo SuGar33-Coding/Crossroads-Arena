685d8279ad7616722e0dd27659864c527e84b9d9
tsbraun1891 <tsbraun1891@gmail.com>
2021-07-30 15:55:22 -0400

    Add basic custom cursor

--- UI/StartMenu/StartMenu.gd
+++ UI/StartMenu/StartMenu.gd
@@ -1,9 +1,12 @@
 extends Control
 
+var cursor = preload("res://Assets/Cursor.png")
+
 onready var playerStats = get_node("/root/PlayerStats")
 
 func _ready():
 	Inventory.resetInventory()
+	Input.set_custom_mouse_cursor(cursor)
 
 func _on_ColorPicker_color_changed(color):
 	playerStats.playerColor = color
